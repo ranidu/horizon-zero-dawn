a82c4db00e6b6b8edd5017e37e6d80a9dcc2f840
Ranidu Nadeesha <ranidunadeesha@gmail.com>
2025-05-21 06:52:16 +0800

    feat: add thumbnail support

--- src/ItemDrawer.jsx
+++ src/ItemDrawer.jsx
@@ -1,5 +1,5 @@
 import { map } from "lodash";
-import { Card, Flex } from "antd";
+import { Card, Flex, Image } from "antd";
 import thumb_1 from '/0.jpeg'
 
 const ItemDrawer = ({ items }) => {
@@ -7,17 +7,21 @@ const ItemDrawer = ({ items }) => {
   if (!items) return;
 
   return (
-    <Flex gap={20}>
+    <Flex gap={20} wrap>
       {map(items, (item, index) => (
         <Card
           key={`card-item-${index}`}
           hoverable
           style={{ width: 240 }}
           cover={
-            <img
+            <div style={{ height: 250, overflow: 'hidden' }}>
+            <Image
               alt="example"
-              src={thumb_1}
+              src={item?.thumbnail}
+              fallback={thumb_1}
+              style={{ width: '100%', height: '100%', objectFit: 'contain' }}
             />
+            </div>
           }
         >
           <Meta title={`${item.itemName} - ${item.unitPrice}`} description={item.itemDesc} />
